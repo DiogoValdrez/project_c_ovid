04c6428bde9986cf784c3e33860987b48bc9a49b
Diogo Valdrez <diogovaldrez@tecnico.ulisboa.pt>
2021-05-21 19:26:08 +0100

    Misc tested

--- main.c
+++ main.c
@@ -7,46 +7,50 @@ int main()
 {
     Country *CountryHead = NULL;
     
-    char country[64], continent[16], country_code[3];
-    unsigned long int population;
+    char country[64] = "Portugal", continent[16] = "Euro", country_code[3] = "POR";
+    unsigned long int population = 10000000;
+    char n_week[8] = "2020-10";
+    int week_cases = 10, week_cases_ratio = 0,  total_cases = 12;
+    CountryHead = create_node(CountryHead, country, country_code, continent, population, n_week, week_cases, week_cases_ratio, total_cases, "cases");
 
-    char n_week[8] = "2020-13";
-    int week_cases = 10, week_cases_ratio = 0,  total_cases = 12;//pode vaer aqui uma verificação se é maior que os casos da semana
+    strcpy(n_week, "2020-13");//pode haver verificação se é numero valido
+    week_cases = 10;
+    week_cases_ratio = 0; 
+    total_cases = 12;//pode vaer aqui uma verificação se é maior que os casos da semana
     int week_deaths = 11, week_deaths_ratio = 1, total_deaths = 13;
 
-    printf("Insere o primeiro pais:\n");
+    /*printf("Insere o primeiro pais:\n");
     scanf("%s", country);
     scanf("%s", country_code);
     scanf("%s", continent);
-    scanf("%lu", &population);
-
+    scanf("%lu", &population);*/
+    //tem de se fazer dados de semana default, para n ficar com dados anteriores
     //atenção ao tamanho das strings
-    //Testar
     //atenção que o valgrind queixa se que estamos a ir a valores não inicializados a seguir a cada print dos nodes
 
     CountryHead = create_node(CountryHead, country, country_code, continent, population, n_week, week_cases, week_cases_ratio, total_cases, "cases");
     CountryHead = create_node(CountryHead, country, country_code, continent, population, n_week, week_deaths, week_deaths_ratio, total_deaths, "deaths");
+
+    strcpy(n_week, "2020-11");
+    week_cases = 10; 
+    week_cases_ratio = 3; 
+    total_cases = 12;
+    week_deaths = 11;
+    week_deaths_ratio = 1;
+    total_deaths = 13;
+    CountryHead = create_node(CountryHead, country, country_code, continent, population, n_week, week_cases, week_cases_ratio, total_cases, "cases");
+    CountryHead = create_node(CountryHead, country, country_code, continent, population, n_week, week_deaths, week_deaths_ratio, total_deaths, "deaths");
+
+    strcpy(country, "Espanha");
+    strcpy(continent, "Euro");
+    strcpy(country_code, "Esp");
+    population = 11000000;
+    CountryHead = create_node(CountryHead, country, country_code, continent, population, n_week, week_cases, week_cases_ratio, total_cases, "cases");
+    CountryHead = create_node(CountryHead, country, country_code, continent, population, n_week, week_deaths, week_deaths_ratio, total_deaths, "deaths");
+
+
+
     //printf("%p\n", CountryHead);
-    /*printf("Insere o segundo pais:\n");
-
-    scanf("%s", country);
-    scanf("%s", country_code);
-    scanf("%s", continent);
-    scanf("%lu", &population);
-
-    NewCountry = create_country(country, country_code, continent, population);
-    CountryHead = add_CEL(CountryHead, NewCountry);
-
-    printf("Insere o terceiro pais, que vai para o inicio:\n");
-
-    scanf("%s", country);
-    scanf("%s", country_code);
-    scanf("%s", continent);
-    scanf("%lu", &population);
-
-    NewCountry = create_country(country, country_code, continent, population);
-    CountryHead = add_CEL(CountryHead, NewCountry);
-    //CountryHead = switch_countries(NewCountry, NewCountry3);*/
 
     print_nodes(CountryHead);
     free_nodes(CountryHead);
